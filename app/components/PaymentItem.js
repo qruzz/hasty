069dcb707b7db9de9ce4711d908f982dab2a3aef
Michael Nissen <m9ssen@gmail.com>
2017-02-24 12:21:42 +0100

    ProfileView is now complete with fully functional database integration.

--- app/components/PaymentItem.js
+++ app/components/PaymentItem.js
@@ -6,15 +6,42 @@ import {
    TextInput
 } from 'react-native'
 
-import { updateUserData } from '../services/Firebase.js'
+import { updateUserData, database } from '../services/Firebase.js'
 
 export default class PaymentItem extends Component {
+
    constructor(props) {
       super(props)
 
       this.state = {
-         address: 'input'
+         input: 'input'
       }
+   }
+
+   componentDidMount() {
+      database.ref('/users/10212492588289260').once('value').then(function(snapshot) {
+         if (this.props.identifier == "address") {
+            if (snapshot.val().address !== "") {
+               this.setState({input: snapshot.val().address})
+            }
+         } else if (this.props.identifier == "city") {
+            if (snapshot.val().city !== "") {
+               this.setState({input: snapshot.val().city})
+            }
+         } else if (this.props.identifier == "area") {
+            if (snapshot.val().area !== "") {
+               this.setState({input: snapshot.val().area})
+            }
+         } else if (this.props.identifier == "country") {
+            if (snapshot.val().country !== "") {
+               this.setState({input: snapshot.val().country})
+            }
+         } else if (this.props.identifier == "card") {
+            if (snapshot.val().card !== "") {
+               this.setState({input: snapshot.val().card})
+            }
+         }
+      }.bind(this))
    }
 
    render() {
@@ -23,11 +50,20 @@ export default class PaymentItem extends Component {
             <Text style={styles.type}>{this.props.type}</Text>
             <TextInput
                style={styles.input}
-               value={this.state.address}
-               onFocus={() => {this.setState({address: ''})}}
-               onChangeText={(text) => {this.setState({address: text})}}
+               defaultValue={this.props.value}
+               value={this.state.input}
+               onFocus={() => {
+                  this.setState({input: ''})
+               }}
+               onChangeText={(text) => {
+                  this.setState({input: text})
+               }}
                // TODO: import the userID of logged in user
-               onEndEditing={() => {updateUserData('10212492588289260', 'address', this.state.address)}}
+               onEndEditing={() => {
+                  console.log(this.props.identifier)
+                  console.log(this.state.input)
+                  updateUserData('10212492588289260', this.props.identifier, this.state.input)
+               }}
             />
          </View>
       )
--- app/scenes/Profile/ProfilePayment.js
+++ app/scenes/Profile/ProfilePayment.js
@@ -3,29 +3,72 @@ import { View } from 'react-native'
 
 import PaymentItem from '../../components/PaymentItem.js'
 
+import { database } from '../../services/Firebase.js'
+import { userID } from '../Authentication/Functions.js'
+
 export default class ProfileContent extends Component {
+   constructor(props) {
+      super(props)
+
+      this.state = {
+         address: 'input',
+         city: 'input',
+         area: 'input',
+         country: 'input',
+         card: 'input'
+      }
+   }
+
+   // componentDidMount() {
+   //    // TODO: Append users with + userID: '/users/' + userID
+   //    database.ref('/users/10212492588289260').once('value').then(function(snapshot) {
+   //       if (snapshot.val().country !== ""){
+   //          // console.log(snapshot.val().country)
+   //          this.setState({country: snapshot.val().country})
+   //          // console.log(this.state.country)
+   //       }
+   //
+   //       if (snapshot.val().address !== "") {
+   //          this.setState({address: snapshot.val().address})
+   //       } else if (snapshot.val().city !== "") {
+   //          this.setState({city: snapshot.val().city})
+   //       } else if (snapshot.val().area !== "") {
+   //          this.setState({area: snapshot.val().area})
+   //       } else if (snapshot.val().country !== "") {
+   //          this.setState({country: snapshot.val().country})
+   //       } else if (snapshot.val().card !== "") {
+   //          this.setState({card: snapshot.val().card})
+   //       }
+   //    }.bind(this))
+   // }
+
    render() {
       return (
          <View>
             <PaymentItem
                type='Address'
-               value='input'
+               value={this.state.address}
+               identifier='address'
             />
             <PaymentItem
                type='City'
-               value='input'
+               value={this.state.city}
+               identifier='city'
             />
             <PaymentItem
                type='State'
-               value='input'
+               value={this.state.area}
+               identifier='area'
             />
             <PaymentItem
                type='Country'
-               value='input'
+               value={this.state.country}
+               identifier='country'
             />
             <PaymentItem
                type='Card no.'
-               value='input'
+               value={this.state.card}
+               identifier='card'
             />
          </View>
       )
